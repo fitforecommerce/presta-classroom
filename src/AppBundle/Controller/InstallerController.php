0661b86fccd0e41319d18fa8be2bd7fca301145e
ediathome <edi@ediathome.de>
2017-03-25 11:52:27 +0100

    add install classes

---
+++ src/AppBundle/Controller/InstallerController.php
@@ -0,0 +1,26 @@
+<?php
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Form\InstallerConfigType;
+
+class InstallerController extends Controller
+{
+
+    /**
+	 * @Route("/install")
+	 * @Route("/install/configure")
+     */
+    public function configure()
+    {
+        $f = $this->createForm(InstallerConfigType::class);
+		# $fv = $f->buildView();
+        return $this->render(
+			'install/configure.html.twig', 
+			array('form' => $f)
+		);
+    }
+}
+?>
