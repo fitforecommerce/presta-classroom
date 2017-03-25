f12570ad777df0d11680d4e9bf00d611baa5f4c6
ediathome <edi@ediathome.de>
2017-03-25 10:30:07 +0100

    reorganize services

---
+++ src/AppBundle/Utils/VersionDownload.php
@@ -0,0 +1,31 @@
+<?php
+namespace AppBundle\Model;
+
+use Symfony\Component\Yaml\Yaml;
+
+class VersionDownload {
+
+	public static function available_versions()
+	{
+		$fp = realpath(__DIR__.'/../../../app/config/prestashop_versions.yml');
+		$l = Yaml::parse(file_get_contents($fp));
+		return $l;
+	}
+	function __construct($ndata) {
+		$this->data = $ndata;
+	}
+	public function is_downloaded()
+	{
+		$arDir = array();
+		$this->assert_download_dir();
+		$d = dir($this->download_target_dir());
+		while (false !== ($entry = $d->read())) {
+			if($entry != '.' && $entry != '..') {
+				array_push($arDir, $entry);
+			}
+		}
+		$d->close();
+		return $arDir;
+	}
+}
+?>
