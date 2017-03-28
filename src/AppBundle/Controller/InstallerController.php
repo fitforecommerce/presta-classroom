27c6461f5a09cd4af859b03ca09ab2a2b68fb71c
ediathome <edi@ediathome.de>
2017-03-28 21:08:26 +0200

    add basic config and execute functionality

--- src/AppBundle/Controller/InstallerController.php
+++ src/AppBundle/Controller/InstallerController.php
@@ -5,22 +5,38 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Form\InstallerConfigType;
+use Symfony\Component\HttpFoundation\Request;
 
 class InstallerController extends Controller
 {
 
     /**
 	 * @Route("/install")
-	 * @Route("/install/configure")
      */
-    public function configure()
+    public function configure(Request $request)
     {
         $f = $this->createForm(InstallerConfigType::class);
-		# $fv = $f->buildView();
+	    $f->handleRequest($request);
+		
+		if ($f->isSubmitted() && $f->isValid()) {
+			$task = $f->getData();
+			return $this->execute($request);
+		}
+
         return $this->render(
 			'install/configure.html.twig', 
 			array('form' => $f->createView())
 		);
     }
+
+    /**
+	 * @Route("/runinstall")
+     */
+	public function execute(Request $request)
+	{
+        return $this->render(
+			'install/execute.html.twig'
+		);
+	}
 }
 ?>
--- src/AppBundle/Form/InstallerConfigType.php
+++ src/AppBundle/Form/InstallerConfigType.php
@@ -5,13 +5,19 @@ use AppBundle\Entity\VersionDownload;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType; 
-use Symfony\Component\Form\Extension\Core\Type\EmailType; 
+
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class InstallerConfigType extends AbstractType {
 	public function buildForm(FormBuilderInterface $builder, array $options) {
-		$builder ->add('server_path', TextareaType::class); 
+		$builder->add('number_of_installations', IntegerType::class);
+		$builder->add('server_path', TextType::class);
+
+		$builder->add('submit', SubmitType::class);
 	}
 	public function configureOptions(OptionsResolver $resolver) {
 		# $resolver->setDefaults(array( 'data_class' => Post::class,)); 
