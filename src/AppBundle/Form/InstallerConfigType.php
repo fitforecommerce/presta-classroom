0ab8544f4823b89913d5cbcf18145a254acd94e4
ediathome <edi@ediathome.de>
2017-04-13 17:19:17 +0200

    add prestashop version select

--- src/AppBundle/Form/InstallerConfigType.php
+++ src/AppBundle/Form/InstallerConfigType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
@@ -14,13 +15,28 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class InstallerConfigType extends AbstractType {
 	public function buildForm(FormBuilderInterface $builder, array $options) {
-		$builder->add('number_of_installations', IntegerType::class);
-		$builder->add('server_path', TextType::class);
+		$builder->add(
+			'presta_version', 
+			ChoiceType::class, 
+			array('choices'  => $this->versions_choice())
+		);
 
+		$builder->add('number_of_installations', IntegerType::class);
+
+		$builder->add('server_path', TextType::class);
 		$builder->add('submit', SubmitType::class);
 	}
 	public function configureOptions(OptionsResolver $resolver) {
 		# $resolver->setDefaults(array( 'data_class' => Post::class,)); 
+	}
+	private function versions_choice()
+	{
+		$rv = [];
+		$av = VersionDownload::available_versions();
+		foreach ($av as $k => $v) {
+			$rv[$v['version']] = $v['version'];
+		}
+		return $rv;
 	}
 }	
 ?>
