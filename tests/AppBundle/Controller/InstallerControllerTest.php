23b252424854a56b391d97fbd62fdc3702dbb396
ediathome <edi@ediathome.de>
2017-04-14 18:16:36 +0200

    implement methods

--- tests/AppBundle/Controller/InstallerControllerTest.php
+++ tests/AppBundle/Controller/InstallerControllerTest.php
@@ -18,35 +18,32 @@ class InstallerControllerTest extends WebTestCase
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Install options', $crawler->filter('h1')->text());
     }
-	public function testInstaller()
+	public function testConfigurationForm()
 	{
         $client = static::createClient();
-		$dp = $client->getContainer()->getParameter('app.presta_versions_download_dir');
-		$td = $this->target_download($dp);
-
-		$this->file_helper()->remove(realpath($dp));
-
-        $crawler = $client->request('GET', '/download/1.7.0.4');
-		$this->assertFileExists(realpath($td['zipped_path']));
-		$this->assertDirectoryExists(realpath($td['unzipped_path']));
+        $crawler = $client->request('GET', '/install');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+		$this->assertCount(1, $crawler->filter('#installer_config_presta_version'));
 	}
+	public function testInstallation()
+	{
+        $client = static::createClient();
+		$dp = $client->getContainer()->getParameter('app.default_shops_dir');
+		$this->file_helper()->remove(realpath($dp));
+		$this->assertDirectoryNotExists($dp.'/shop1');
+
+        $crawler = $client->request('GET', '/install');
+		$form = $crawler->selectButton('Submit')->form();
+
+		$client->submit($form);
+		$this->assertDirectoryExists($dp.'/shop1');
+	}
+
+
 	private function file_helper()
 	{
 		if($this->file_helper) return $this->file_helper;
 		$this->file_helper = new FileHelper();
 		return $this->file_helper;
-	}
-	private function target_download($dp)
-	{
-		$version = '1.7.0.4';
-		return array(
-			'version' 			=> $version,
-			'zipped_path'		=> $dp.'/'.$version.'.zip',
-			'unzipped_path'		=> $dp.'/'.$version.'.unzipped'
-		);
-	}
-	private function remove_dir($file)
-	{
-		
 	}
 }
