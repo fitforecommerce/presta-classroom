489401ec9bc165d5e47467c71bd671c1237dc49a
Martin Kolb <edi@ediathome.de>
2019-01-14 14:35:04 +0100

    add cleanup test

--- tests/InstallerTest.php
+++ tests/InstallerTest.php
@@ -10,6 +10,8 @@ final class InstallerTest extends TestCase
     public function setUp()
     {
         parent::setUp();
+        $this->tdir = 'public/testshops';
+        system("rm -rf $this->tdir");
     }
     /**
      * @expectedException Exception
@@ -26,48 +28,41 @@ final class InstallerTest extends TestCase
     }
     public function testShopDirsCreated()
     {
-      $tdir = 'public/testshops';
-      system("rm -rf $tdir");
-
-      $this->assertFileNotExists($tdir);
+      $this->assertFileNotExists($this->tdir);
 
       $c = new InstallerConfig();
-      $c->set('server_path', $tdir);
+      $c->set('server_path', $this->tdir);
 
       $r = new Installer($c);
       $r->assert_dirs();
 
-      $this->assertFileExists($tdir);
+      $this->assertFileExists($this->tdir);
     }
     public function testInstallersUnzipped()
     {
       ini_set('max_execution_time', 60);
-      $tdir = 'public/testshops';
-      system("rm -rf $tdir");
 
       $c = new InstallerConfig();
-      $c->set('server_path', $tdir);
+      $c->set('server_path', $this->tdir);
       $c->set('number_of_installations', 1);
 
       $r = new Installer($c);
       $r->assert_dirs();
       $r->unzip_src();
 
-      $this->assertFileExists($tdir);
-      $this->assertFileExists($tdir.'/shop1');
-      $this->assertFileExists($tdir.'/shop1/install');
-      $this->assertFileExists($tdir.'/shop1/admin');
-      $this->assertFileExists($tdir.'/shop1/init.php');
-      $this->assertFileExists($tdir.'/shop1/index.php');
+      $this->assertFileExists($this->tdir);
+      $this->assertFileExists($this->tdir.'/shop1');
+      $this->assertFileExists($this->tdir.'/shop1/install');
+      $this->assertFileExists($this->tdir.'/shop1/admin');
+      $this->assertFileExists($this->tdir.'/shop1/init.php');
+      $this->assertFileExists($this->tdir.'/shop1/index.php');
     }
     public function testInstallersCopied()
     {
       ini_set('max_execution_time', 60);
-      $tdir = 'public/testshops';
-      system("rm -rf $tdir");
 
       $c = new InstallerConfig();
-      $c->set('server_path', $tdir);
+      $c->set('server_path', $this->tdir);
       $c->set('number_of_installations', 3);
 
       $r = new Installer($c);
@@ -75,13 +70,42 @@ final class InstallerTest extends TestCase
       $r->unzip_src();
       $r->copy_folders();
 
-      $this->assertFileExists($tdir);
+      $this->assertFileExists($this->tdir);
       for ($i=1; $i < 4; $i++) { 
-        $this->assertFileExists($tdir."/shop$i");
-        $this->assertFileExists($tdir."/shop$i/install");
-        $this->assertFileExists($tdir."/shop$i/admin");
-        $this->assertFileExists($tdir."/shop$i/init.php");
-        $this->assertFileExists($tdir."/shop$i/index.php");
+        $this->assertFileExists($this->tdir."/shop$i");
+        $this->assertFileExists($this->tdir."/shop$i/install");
+        $this->assertFileExists($this->tdir."/shop$i/admin");
+        $this->assertFileExists($this->tdir."/shop$i/init.php");
+        $this->assertFileExists($this->tdir."/shop$i/index.php");
+      }
+    }
+    public function testCLIInstallerRun()
+    {
+      
+    }
+    public function testCleanedUp()
+    {
+      ini_set('max_execution_time', 60);
+      system("mkdir $this->tdir");
+      $this->assertFileExists($this->tdir);
+
+      for ($i=1; $i < 4; $i++) { 
+        system("mkdir $this->tdir/shop$i");
+        system("mkdir $this->tdir/shop$i/install");
+        system("mkdir $this->tdir/shop$i/admin");
+      }
+
+      $c = new InstallerConfig();
+      $c->set('server_path', $this->tdir);
+      $c->set('number_of_installations', 3);
+
+      $r = new Installer($c);
+      $r->cleanup();
+
+      for ($i=1; $i < 4; $i++) { 
+        $this->assertFileNotExists($this->tdir."/shop$i/install");
+        $this->assertFileNotExists($this->tdir."/shop$i/admin");
+        $this->assertFileExists($this->tdir."/shop$i/admin123");
       }
     }
 }
